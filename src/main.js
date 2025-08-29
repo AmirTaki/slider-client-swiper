38a1da8660a734c1301f25d5e38faf83a4b31cbf
amir <amirtaki623@gmail.com>
2025-08-29 22:06:10 +0330

    part 12

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ var swiper = new Swiper (".swiper-container", {
     effect : "coverflow",
     grabCursor: true,
     centeredSlides: true,
-
+    slidesPerView : 'auto',
+    coverflowEffect : {
+        
+    }
 })
